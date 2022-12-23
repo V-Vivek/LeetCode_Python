4b0f5450fd1028ddc55197558266a677f83473b9
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-12-23 22:09:08 +0530

    Solution

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        check = []
+        result = False
+
+        for _ in s:
+            if _ == '{':
+                check.append(_)
+            elif _ == '}' and len(check) > 0:
+                if check[-1] != '{':
+                    return result
+                check.pop()
+            elif _ == '[':
+                check.append(_)
+            elif _ == ']' and len(check) > 0:
+                if check[-1] != '[':
+                    return result
+                check.pop()
+            elif _ == '(':
+                check.append(_)
+            elif _ == ')' and len(check) > 0:
+                if check[-1] != '(':
+                    return result
+                check.pop()
+            else:
+                return result
+
+        if check == []:
+            result = True
+        return result
