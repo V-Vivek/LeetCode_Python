b6bd13b901fbf93edf2f5280afe892a0b82924c1
V-Vivek <jadhav.vivek.18.3@gmail.com>
2022-12-27 07:03:09 +0530

    Solution

--- 66. Plus One.py
+++ 66. Plus One.py
@@ -1,5 +1,6 @@
 # Question: https://leetcode.com/problems/plus-one/
 
+# Solution #1
 class Solution:
     def plusOne(self, digits: list[int]) -> list[int]:
         result = ''
@@ -11,3 +12,22 @@ class Solution:
         for n in result:
             oneplus.append(int(n))
         return oneplus
+
+
+# Solution #2
+class Solution:
+    def plusOne(self, digits: list[int]) -> list[int]:
+        was9 = False
+        i = -1
+        while i > (len(digits) * -1) - 1:
+            if digits[i] == 9:
+                digits[i] = 0
+                was9 = True
+            else:
+                digits[i] += 1
+                was9 = False
+                break
+            i -= 1
+        if was9:
+            digits = [1] + digits
+        return digits
